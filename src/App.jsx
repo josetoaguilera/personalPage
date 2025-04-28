40920f5c77bd3a69540d32e1d34db020c830d4b1
Jose Tomas <jose.aguilera@lvaindices.com>
2025-04-28 12:34:47 -0400

    FIX: browser paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
@@ -7,12 +7,14 @@ import ProjectsPage from './pages/ProjectsPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path='/projects' element={<ProjectsPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
