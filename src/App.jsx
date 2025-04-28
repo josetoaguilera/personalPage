04ff78ab7bb876c3c9f62b9266f96e5978254925
Jose Tomas <jose.aguilera@lvaindices.com>
2025-04-28 12:16:04 -0400

    FIX: paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '@/pages/HomePage';
+import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ProjectsPage from './pages/ProjectsPage';
@@ -8,7 +8,7 @@ import ProjectsPage from './pages/ProjectsPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="PersonalPage/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path='/projects' element={<ProjectsPage />} />
